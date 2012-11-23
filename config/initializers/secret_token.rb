26d5ed0f1393c6043d582e27da2037332c5e5222
Steven Ko <chunyuko85@gmaill.com>
2012-11-23 16:52:06 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoutubeCrawl::Application.config.secret_token = '61bee59bd6a847993c4f71cfabf11f70909148ba9393888ccc541af5ae446122c897764dd6b2e2620fc618070b1b102e9400c8e35c3f069f7b1c7f09d4246ca6'
