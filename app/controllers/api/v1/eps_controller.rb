8490d9c84967eb10f5751cc65f1578fb452bb19e
Steven Ko <chunyuko85@gmaill.com>
2012-12-03 15:52:54 +0800

    ep num DESC

--- app/controllers/api/v1/eps_controller.rb
+++ app/controllers/api/v1/eps_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::EpsController < Api::ApiController
 
   def index
     drama_id = params[:drama_id]
-    eps = Ep.drama_eps(drama_id)
+    eps = Ep.by_num_order.drama_eps(drama_id)
     render :json => eps
   end
 end
--- app/models/ep.rb
+++ app/models/ep.rb
@@ -2,6 +2,7 @@ class Ep < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :drama, :counter_cache => true
   has_many :youtube_sources
-
+  
+  scope :by_num_order, order('num DESC')
   scope :drama_eps, lambda { |drama_id| where('drama_id = (?)', drama_id).select('id,num') }
 end
