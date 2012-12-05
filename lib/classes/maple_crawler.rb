cb763d939f9631097cb30faa3c4ecb0054c8153c
Steven Ko <chunyuko85@gmaill.com>
2012-12-05 11:12:24 +0800

    parse drama

--- lib/classes/maple_crawler.rb
+++ lib/classes/maple_crawler.rb
@@ -32,47 +32,14 @@ class MapleCrawler
     puts @page_url + "  do not have summary" unless summary
     
     intro = summary.to_html
-    # intro = intro.gsub("<p>","")
-    # intro = intro.gsub("</p>","")
-    # intro = intro.gsub("<b>","")
-    # intro = intro.gsub("</b>","")
     intro = intro.gsub("<br>","\n")
     n = Nokogiri::HTML(intro)
     intro = n.text
-    # brs = body_node.css("text")
-    # actors = ""
-    # directors = ""
-    # intro = ""
-    
-    # index = summary.index("編劇")
-    # intro = summary[0..index]  if index
-    # index_directors = summary.index("导演")
-    # intro = summary[0..index_directors]  if intro.blank? if index_directors
-    # index_actors = summary.index("演出")
-    # intro = summary[0..index_actors]  if intro.blank? if index_actors
-    
-    # if index_actors
-    #   actors = summary[index_actors..summary.length]
-    #   actors = actors.split(":")[1].strip
-    # end
-
-    # if index_actors && index_directors
-    #   directors = summary[index_directors..index_actors-1]
-    #   directors = directors.split(":")[1].strip
-    # end
-
-    # arrays.each do |text|
-    #   if text.include?("演出")
-    #     actors = text
-    #     actors = actors.split(":")[1]
-    #   end
-    #   if text.include?("导演")
-    #     directors = text
-    #     directors = directors.split(":")[1]
-    #   end
-    # end
 
     drama = Drama.find_by_name(titles[0])
+    puts " drama : #{drama.name} #{drama.name_en} #{drama.poster_url} #{drama.introduction} #{drama.release_date} #{drama.area.name} #{drama.directors} #{drama.actors}" if drama
+    return if drama
+
     drama = Drama.new unless drama
     drama.name = titles[0]
     drama.name_en = titles[1]
