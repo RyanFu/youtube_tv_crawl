824b95ab71559b141e9fdddb365ab1af37752b56
Steven Ko <chunyuko85@gmaill.com>
2012-12-21 14:12:54 +0800

    new dramas_info

--- app/controllers/api/v1/dramas_controller.rb
+++ app/controllers/api/v1/dramas_controller.rb
@@ -12,6 +12,13 @@ class Api::V1::DramasController < Api::ApiController
     render :json => @dramas.to_json
   end
 
+  def new_dramas_info
+    dramas_id = params[:dramas_id]
+    dramas_id_array = dramas_id.split(",")
+    @dramas = Drama.select('id,name,introduction,poster_url,eps_num_str,release_date,area_id').where(['id in (?)', dramas_id_array])
+    render :json => @dramas.to_json
+  end
+
   def update
     drama = Drama.find(params[:id])
     drama.views += 1
