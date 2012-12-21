c585dca022733204a42ee6f502f59f58efa76fa3
Steven Ko <chunyuko85@gmaill.com>
2012-12-21 13:27:51 +0800

    drama_info num_str

--- app/controllers/api/v1/dramas_controller.rb
+++ app/controllers/api/v1/dramas_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::DramasController < Api::ApiController
   def dramas_info
     dramas_id = params[:dramas_id]
     dramas_id_array = dramas_id.split(",")
-    @dramas = Drama.select('id,name,introduction,poster_url,eps_count,release_date,area_id').where(['id in (?)', dramas_id_array])
+    @dramas = Drama.select('id,name,introduction,poster_url,eps_num_str,release_date,area_id').where(['id in (?)', dramas_id_array])
     render :json => @dramas.to_json
   end
 
