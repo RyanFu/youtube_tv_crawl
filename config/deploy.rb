f9e3d705e78174556828aff63f8fcc9eedc8de09
RayWoo <raywu07@gmail.com>
2013-02-04 00:27:32 +0800

    set :repository

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, "youtube_crawl"
 set :rails_env, "production"
 
 set :branch, "master"
-set :repository,  "https://github.com/StevenKo/youtube_tv_crawl.git"
+set :repository,  "https://github.com/abooyaya/youtube_tv_crawl"
 set :scm, "git"
 set :user, "apps" # 一個伺服器上的帳戶用來放你的應用程式，不需要有sudo權限，但是需要有權限可以讀取Git repository拿到原始碼
 set :port, "222"
