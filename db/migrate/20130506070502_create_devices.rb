16389ecefc86766f14a94bbc510432349f919ccc
ben <ben@ubuntuBen.dynamic.kbronet.com.tw>
2013-05-06 17:34:35 +0800

    correct typo

--- db/migrate/20130506070502_create_devices.rb
+++ db/migrate/20130506070502_create_devices.rb
@@ -1,8 +1,8 @@
 class CreateDevices < ActiveRecord::Migration
   def change
     create_table :devices do |t|
-      t.string :registration_id, default => ''
-      t.string :device_id, default => ''
+      t.string :registration_id, :default => ''
+      t.string :device_id, :default => ''
       t.timestamps
     end
   end
