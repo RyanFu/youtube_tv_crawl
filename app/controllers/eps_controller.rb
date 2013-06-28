375e53bd0b172b25a82e17d1d743773ed37d5d67
ben <ben@ubuntuBen.dynamic.kbronet.com.tw>
2013-06-28 12:27:09 +0800

    unlock some drama of parser

--- app/controllers/eps_controller.rb
+++ app/controllers/eps_controller.rb
@@ -2,7 +2,7 @@ class EpsController < ApplicationController
   # GET /Eps
   # GET /Eps.json
   def index
-    @eps = Ep.paginate(:page => params[:page], :per_page => 20)
+    @eps = Ep.paginate(:page => params[:page], :per_page => 20).order("id DESC")
 
     respond_to do |format|
       format.html # index.html.erb
