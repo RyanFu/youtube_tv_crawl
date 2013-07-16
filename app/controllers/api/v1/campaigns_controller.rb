0acd36e1d262a52addd12c8c9a8c014ac39bcbd5
RayWoo <raywu07@gmail.com>
2013-07-16 20:49:51 +0800

    add ticket_count

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -3,8 +3,17 @@ class Api::V1::CampaignsController < ApplicationController
 
   def index
     campaigns = Campaign.where("is_show = 1").select("title,imageurl,description,id")
-
-    render :json => campaigns	
+    response = []
+    campaigns.each do |campaign|
+      n ={}
+      n["title"] = campaign.title
+      n["imageurl"] = campaign.imageurl
+      n["description"] = campaign.id
+      ticket_count = Ticket.where("campaign_id = ?",campaign.id).count + 2000
+      n["ticket_count"] = ticket_count
+      response << n 
+    end
+    render :status=>200, :json => response.to_json
   end	
 
 end
