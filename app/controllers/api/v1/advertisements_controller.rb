c5ff60d67fd2ac3ae7b9762a7a7dd4839c184f5e
RayWoo <raywu07@gmail.com>
2013-07-19 10:52:51 +0800

    modify ad remove db

--- app/controllers/api/v1/advertisements_controller.rb
+++ app/controllers/api/v1/advertisements_controller.rb
@@ -1,25 +1,32 @@
+# encoding: utf-8
 class Api::V1::AdvertisementsController < ApplicationController
 
   def index
     type_id = params[:type_id].to_i
+    response = []
     if type_id == 0
-      advs = Advertisement.where("is_show = ? AND type_id = ?",1,type_id)
-      if advs[0].title == nil and advs[0].description == nil 
-        adv = Advertisement.where("is_show = ? AND type_id = ?",1,type_id).select("imageurl")
-      else
-      	adv = Advertisement.where("is_show = ? AND type_id = ?",1,type_id).select("title,imageurl,description")
-      end
-    elsif type_id == 1
-      adv = Advertisement.where("is_show = ? AND type_id = ?",1,type_id).select("imageurl")
+      n ={}
+      n["description"] = "用戶獨享優惠165元"
+      n["imageurl"] = "http://pic.pimg.tw/jumplives/1374117884-921708010.jpg?v=1374117915"
+      n["title"] = "火影忍者 全球特展"
 
+     # n["imageurl"] = "http://pic.pimg.tw/jumplives/1374117890-655812443.jpg?v=1374117917"
+      response << n  
+    elsif type_id == 1
+      n ={}
+     # n["imageurl"] = "http://pic.pimg.tw/jumplives/1374117891-4289730248.jpg?v=1374117917"
+     # response << n
     elsif type_id == 2
-      
-      adv = Advertisement.where("is_show = ? AND type_id = ?",1,type_id).select("title,imageurl")	
+      n ={}
+      n["imageurl"] = "http://pic.pimg.tw/jumplives/1374117884-921708010.jpg?v=1374117915"
+      n["title"] = "火影忍者 特展"
+      response << n
     else
-      adv = Advertisement.where("is_show = ? AND type_id = ?",1,type_id).select("title,imageurl,description") 		
+      n ={}
+    #  response << n
     end
 
-    render :json => adv
+    render :json => response.to_json
   end
 
 
