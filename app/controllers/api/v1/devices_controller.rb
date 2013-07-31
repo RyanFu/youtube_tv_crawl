e26f56cc13614b0ede3746b30d5f351fb929b186
ben <ben@ubuntuBen.dynamic.kbronet.com.tw>
2013-07-31 16:48:55 +0800

    remove add device to gcm temporary

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -1,24 +1,25 @@
 class Api::V1::DevicesController < ApplicationController
 
   def create
-  	registration_id = params[:registration_id]
-  	device_id = params[:device_id]
+  	#registration_id = params[:registration_id]
+  	#device_id = params[:device_id]
     
-    @device = Device.where("registration_id = ?", registration_id)[0]
+    #@device = Device.where("registration_id = ?", registration_id)[0]
     
-    if @device
+    #if @device
       
-    else
-      @device = Device.new
-      @device.registration_id = registration_id
-      @device.device_id = device_id
-    end  
+    #else
+    #  @device = Device.new
+    #  @device.registration_id = registration_id
+    #  @device.device_id = device_id
+    #end  
    
-    if @device.save
-      render :status=>200, :json=>{:message => "success"}
-    else
-      render :status=>401, :json=>{:message=> "create fail" }
-    end
+    #if @device.save
+    #  render :status=>200, :json=>{:message => "success"}
+    #else
+    #  render :status=>401, :json=>{:message=> "create fail" }
+    #end
+    render :status=>200, :json=>{:message => "success"}
 
   end
 
