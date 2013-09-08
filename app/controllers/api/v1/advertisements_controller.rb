5161ef18212d1a20ed8cac3dd04fd89aabee02c4
RayWoo <raywu07@gmail.com>
2013-09-08 23:07:58 +0800

    modify ad bug

--- app/controllers/api/v1/advertisements_controller.rb
+++ app/controllers/api/v1/advertisements_controller.rb
@@ -5,7 +5,9 @@ class Api::V1::AdvertisementsController < ApplicationController
     type_id = params[:type_id].to_i
     if type_id == 0
       advs = Advertisement.where("is_show = ? AND type_id = ?",1,type_id)
-      if advs[0].title == nil and advs[0].description == nil 
+      if advs == []
+        adv = []
+      elsif advs[0].title == nil and advs[0].description == nil 
         adv = Advertisement.where("is_show = ? AND type_id = ?",1,type_id).select("imageurl")
       else
         adv = Advertisement.where("is_show = ? AND type_id = ?",1,type_id).select("title,imageurl,description")
